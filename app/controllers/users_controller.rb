688f5ba6e9322720d29109239253029db8c77730
Duyet <duyet.nguyen@outlook.com>
2015-08-24 09:07:08 +0700

    37347 edit and view user profile

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,27 @@
 class UsersController < ApplicationController
+  before_action :load_user, only: [:edit, :update, :show]
+
   def show
-    @user = User.find_by id: params[:id]
+  end
+
+  def edit
+  end
+
+  def update
+    if @user.update_attributes user_params
+      flash[:notice] = t"application.notice.user_updated"
+      redirect_to user_path
+    else
+      render "edit"
+    end
+  end
+
+  private
+  def load_user
+    @user = User.find params[:id]
+  end
+
+  def user_params
+    params.require(:user).permit :name, :email, :role
   end
 end
