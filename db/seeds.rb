2ea8c3186af90748c057d97df6f2aab826b940cb
tran chinh thuc <thuctc91@gmail.com>
2015-08-27 09:52:12 +0700

    Start-finish course

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,1 +1,9 @@
-User.create(name: "Duyet", role: "supervisor", email: "duyet.vn@gmail.com", password: "12341234", password_confirmation: "12341234")
+User.create(name: "Duyet", role: "supervisor",
+  email: "duyet.vn@gmail.com",
+  password: "12341234", password_confirmation: "12341234")
+User.create(name: "thuc", role: "normal",
+  email: "thuc.vn@gmail.com",
+  password: "12341234", password_confirmation: "12341234")
+User.create(name: "Nam", role: "normal",
+  email: "nam.vn@gmail.com",
+  password: "12341234", password_confirmation: "12341234")
